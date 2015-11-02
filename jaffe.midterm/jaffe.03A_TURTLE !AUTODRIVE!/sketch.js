e8863756c6481aabd27d92965b5ed9905cb964b7
holdenjaffe <Sam@172-16-15-237.DYNAPOOL.NYU.EDU>
2015-11-02 14:53:57 -0500

    midterm 2.0

--- jaffe.midterm/jaffe.03A_TURTLE !AUTODRIVE!/sketch.js
+++ jaffe.midterm/jaffe.03A_TURTLE !AUTODRIVE!/sketch.js
@@ -23,29 +23,110 @@
 // might be done.  :)
 
 var x, y; // some variables for the current position of the turtle
-var currentangle = 270; // we start out facing north
+var currentangle = 0; // we start out facing north
 var step = 20; // how many pixels do we move forward when we draw?
-var angle = 45; // how many degrees do we turn with '+' and '-'
+var angle = 90; // how many degrees do we turn with '+' and '-'
 var uni; 
 
 function preload() {
   uni = loadImage('./data/unicorn.jpg');
 }
 
+
+// LINDENMAYER STUFF (L-SYSTEMS)
+var thestring = 'A'; // "axiom" or start of the string
+var numloops = 5; // how many iterations of the L-system to pre-compute
+var therules = []; // array for rules
+therules[0] = ['A', 'FF−F+F+FF−F−F+F+BB-A']; // first rule
+therules[1] = ['B', 'F−F−F−F+AA-B']; // second rule
+
+
+var whereinstring = 0; // where in the L-system are we drawing right now?
+
+// THIS RUNS WHEN WE HIT GO
 function setup()
 {
-  createCanvas(800, 600);
-  background(255);
-  stroke(0, 0, 0, 255);
+  createCanvas(800, 600); // this is the size of the window
+  background(255); // background to white
+  stroke(0, 0, 0, 255); // draw in black
   
-  // start our turtle in the middle of the screen
-  x = width/2;
-  y = height/2;
+  // start the x and y position at lower-left corner
+  x = 0;
+  y = 0;
+  
+  // COMPUTE THE L-SYSTEM
+  for(var i = 0;i<numloops;i++) {
+    thestring = lindenmayer(thestring); // do the stuff to make the string
+  }
+  console.log(thestring); // comment this out if it's slowing you down
+  
 }
 
+// DO THIS EVERY FRAME
 function draw()
 {
- 
+  
+  // draw the current character in the string:
+  drawIt(thestring.charAt(whereinstring)); 
+  
+  // increment the point for where we're reading the string.
+  // wrap around at the end.
+  whereinstring++;
+  if(whereinstring>thestring.length-1) whereinstring = 0;
+
+}
+
+// interpret an L-system
+function lindenmayer(s)
+{
+  var outputstring = ''; // start a blank output string
+  
+  // go through the string, doing rewriting as we go
+  
+  // iterate through 'therules' looking for symbol matches:
+  for(var i=0;i<s.length;i++) // every symbol in 's'
+  {
+    var ismatch = 0; // by default, no match
+    for(var j = 0;j<therules.length;j++) // every rule in 'therules'
+    {
+      if(s.charAt(i)==therules[j][0]) // MATCH!
+      {
+        outputstring+=therules[j][1]; // write substitution
+        ismatch = 1; // we have a match, so don't copy over symbol
+        break; // get outta this for() loop
+      }
+    }
+    // if nothing matches in 'therules' array, just copy the symbol over.
+    if(ismatch===0) outputstring+= s.charAt(i); 
+  }
+  
+  return(outputstring); // send out the modified string
+}
+
+// this is a custom function that draws turtle commands
+function drawIt(k)
+{
+  if(k=='F') // draw forward
+  {
+    // polar to cartesian transformation based on step and currentangle:
+    var x1 = x + step*cos(radians(currentangle));
+    var y1 = y + step*sin(radians(currentangle));
+    line(x, y, x1, y1); // connect the old and the new
+    // update the turtle's position:
+    x = x1;
+    y = y1;
+  }
+  else if(k=='+')
+  {
+   currentangle+=angle; // turn left
+  }
+  else if(k=='-')
+  {
+   currentangle-=angle; // turn right   
+  }
+   
+  // DRAW EVERYTHING:
+
   // give me some random color values:
   var r = random(128, 255);
   var g = random(0, 192);
@@ -60,10 +141,8 @@ function draw()
   radius = radius/3;
   
   // draw the stuff:
-//  fill(r, g, b, a); // interior fill color
-  //ellipse(x, y, radius, radius); // circle that chases the mouse
-  image(uni, x-50, y-50, radius+90, radius+90);
-
+  fill(r, g, b, a); // interior fill color
+  image(uni, x, y, radius+50, radius+50); // circle that chases the mouse
 }
 
 function keyTyped()
@@ -116,3 +195,4 @@ function keyTyped()
     y = y1;
   }
 }
+
